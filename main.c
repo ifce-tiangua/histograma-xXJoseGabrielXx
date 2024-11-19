6599500a0b38822668ec99db60eff103b568750a
xXJoseGabrielXx <132611883+xXJoseGabrielXx@users.noreply.github.com>
2024-11-18 22:29:49 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,51 +1,50 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void povoa_historiograma(char* h[], int valor, int categoria){
-    //povoar com * a categoria com seu respectivo valor\\
-    
-    for(int i = 0; i < valor; i++){
+void povoa_historiograma(char *h[], int valor, int categoria) {
+    for (int i = 0; i < valor; i++) {
         h[categoria][i] = '*';
-
-}
-}
-
-void imprimir_historiagrama(char* h[], int valores[], int categorias){
-    
-    for(int i = 0; i < categorias; i++){
-    int j;
-        printf("%d",valores[i]);
-        for(int j = 0; j < valores[i]; j++){
-        printf("%c", h[i][j]);
     }
-    printf("\n");
-        
-    }
-    
 }
 
-int main(){
+void imprimir_historiograma(char *h[], int valores[], int categorias) {
+    for (int i = 0; i < categorias; i++) {
+        printf("%d ", valores[i]);
+        for (int j = 0; j < valores[i]; j++) {
+            printf("%c", h[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+int main() {
     int categorias;
     scanf("%d", &categorias);
-    
+
     int valores[categorias];
     char *historiograma[categorias];
-    
-    for(int i = 0; i < categorias; i++){
-        scanf("%d", &valores[i]);
-        
-        if(valores[i] == 0){
-            historiograma[i] = NULL;
-        }else{
-            historiograma[i] = (char *)malloc(valores[i] * sizeof(char));
-            povoa_historiograma(historiograma,valores[i], i);
-            }
-            
-        }
-        imprimir_historiagrama(historiograma, valores, categorias);
-for(int i = 0; i < categorias; i++)
-If (historiograma[i] !=NULL)
-free(historiograma[i]);
 
-        return 0 ;
+    for (int i = 0; i < categorias; i++) {
+        scanf("%d", &valores[i]);
+        if (valores[i] == 0) {
+            historiograma[i] = NULL;
+        } else {
+            historiograma[i] = (char *)malloc(valores[i] * sizeof(char));
+            if (historiograma[i] == NULL) {
+                return 1;
+            }
+            povoa_historiograma(historiograma, valores[i], i);
+        }
     }
+
+    imprimir_historiograma(historiograma, valores, categorias);
+
+    for (int i = 0; i < categorias; i++) {
+        if (historiograma[i] != NULL) {
+            free(historiograma[i]);
+        }
+    }
+
+    return 0;
+}
+
